d57efc1797472b537f87b15a3e4d6e8597511b7d
whoyoux <48690153+whoyoux@users.noreply.github.com>
2020-05-05 11:58:17 +0200

    Some fix

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    basicURL: "http://turniejevalorant.herokuapp.com",
+    basicURL: "https://turniejevalorant.herokuapp.com",
   },
   mutations: {},
   actions: {},
